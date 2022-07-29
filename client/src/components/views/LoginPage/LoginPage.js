33df584051e8b276d890c9068c1d024ebaa8d322
MinHyuk Kim <rhrlwld@gmail.com>
2022-07-29 22:37:43 +0900

    complete login-logout

---
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -0,0 +1,68 @@
+import { Axios } from 'axios'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_actions.js';
+import { useNavigate } from 'react-router-dom';
+
+
+function LoginPage() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [Email, setEmail] = useState("")
+  const [Password, setPassword] = useState("")
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value)
+  }
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value)
+  }
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    let body = {
+      email: Email,
+      password: Password
+    }
+
+    dispatch(loginUser(body))
+      .then(response => {
+        if (response.payload.loginSuccess) {
+          navigate('/') // 리액트에서 페이지를 이동시킬때 사용!!
+        } else {
+          alert('Error')
+        }
+      })
+
+
+  }
+
+  return (
+    <div
+      style={{
+        display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
+      }}>
+      <form style={{ display: 'flex', flexDirection: 'column' }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+
+
+        <br />
+        <button type="submit">
+          Login
+        </button>
+
+      </form>
+    </div>
+  )
+}
+
+export default LoginPage
--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const config = require('./config/key');
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //application/json
 app.use(bodyParser.json());
@@ -19,57 +19,64 @@ app.use(cookieParser());
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
-    .catch(err => console(err))
+  .catch(err => console(err))
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/api/hello', (req, res) => {
+  res.send("안녕하세요.")
+})
+
+
+
+
 app.post('/api/users/register', (req, res) => {
 
-    // 회원가입 할때 필요한 정보들을 client에서 가져오면
-    // 그것들을 DB에 넣어준다.
+  // 회원가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 DB에 넣어준다.
 
-    const user = new User(req.body)
+  const user = new User(req.body)
 
-    user.save((err, doc) => {
-      if(err) return res.json({ success: false, err})
-      return res.status(200).json({ // status(200): 성공했다는 표시
-        success: true
-      })
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({ // status(200): 성공했다는 표시
+      success: true
     })
+  })
 })
 
 app.post('/api/users/login', (req, res) => {
   // 요청된 이메일을 DB에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
-    if(!user) {
+    if (!user) {
       return res.json({
         loginSuccess: false,
         message: "제공된 이메일에 해당되는 유저가 없습니다."
       })
     }
 
-  // 요청된 이메일이 DB에 있다면 비밀번호가 올바른지 확인
-  user.comparePassword(req.body.password, (err, isMatch) => {
-    if(!isMatch)
-    return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
-  
-  // 비밀번호까지 맞다면 토큰을 생성하기.
+    // 요청된 이메일이 DB에 있다면 비밀번호가 올바른지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
 
-  user.generateToken((err, user) => {
-    if(err) return res.status(400).send(err);
+      // 비밀번호까지 맞다면 토큰을 생성하기.
 
-    // 토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지
-    res.cookie("x_auth", user.token)
-    .status(200)
-    .json({ loginSuccess: true, userId: user._id})
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
 
+        // 토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지
+        res.cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id })
+
+      })
+
+    })
   })
-
-  })
-})
 })
 
 app.get('/api/users/auth', auth, (req, res) => {
@@ -77,7 +84,7 @@ app.get('/api/users/auth', auth, (req, res) => {
   // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말
   res.status(200).json({
     _id: req.user._id,
-    isAdmin: req.user.role === 0? false : true,
+    isAdmin: req.user.role === 0 ? false : true,
     isAuth: true,
     email: req.user.email,
     name: req.user.name,
@@ -89,10 +96,10 @@ app.get('/api/users/auth', auth, (req, res) => {
 })
 
 app.get('/api/users/logout', auth, (req, res) => {
-  User.findOneAndUpdate({ _id: req.user._id }, 
-    { token: ""}
-    ,(err, user) => {
-      if(err) return req.json({ success: false, err });
+  User.findOneAndUpdate({ _id: req.user._id },
+    { token: "" }
+    , (err, user) => {
+      if (err) return req.json({ success: false, err });
       return res.status(200).send({
         success: true
       })
